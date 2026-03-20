6b17f2031230bc73f5a86ced755e9338ea87dd68
nexocuba <nexocuba2026@gmail.com>
2026-03-20 17:25:15 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// Verificar sesión
+const user = supabase.auth.getUser();
+if (!user) {
+  window.location.href = "login.html";
+}
 function toggleMenu(){
   document.getElementById("menu").classList.toggle("show");
 }
